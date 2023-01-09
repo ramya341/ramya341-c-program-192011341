9ae36c817ffd87f469e851e8cdc7d213cb6d122c
ramya341 <122247132+ramya341@users.noreply.github.com>
2023-01-09 22:39:06 +0530

    Update day 1 pythagorean_triplet.c

--- day 1 pythagorean_triplet.c
+++ day 1 pythagorean_triplet.c
@@ -14,3 +14,27 @@ printf("%d , %d , %d\n",a,b,c); }
 }
 }
 }
+
+output
+Enter the range : 12
+The Pythogorean Triplets in the given range are :
+3 , 4 , 5
+6 , 8 , 10
+
+Enter the range : 1.5
+The Pythogorean Triplets in the given range are :
+
+
+Enter the range : 25
+The Pythogorean Triplets in the given range are :
+3 , 4 , 5
+5 , 12 , 13
+6 , 8 , 10
+7 , 24 , 25
+8 , 15 , 17
+9 , 12 , 15
+12 , 16 , 20
+15 , 20 , 25
+
+A=-10
+invalid
