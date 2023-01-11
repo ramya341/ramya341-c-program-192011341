5b44b2dfcc0bac8b476ef44ffc96ac6153f008f1
ramya341 <122247132+ramya341@users.noreply.github.com>
2023-01-11 11:03:35 +0530

    Create day 2 q10.c

---
+++ day 2 q10.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+main()
+{
+    	int n,a[20],i,j;
+  printf("Enter number of elements of the array :");
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+		printf("Enter an element of the array:");
+		scanf("%d",&a[i]);
+	}
+    int min_index;
+    for(i = 0; i < n - 1; i++) {
+        min_index = i;
+        for(j = i + 1; j < n; j++) {
+            if(a[min_index] > a[j]) {
+                min_index = j;
+            }
+        }
+        if(min_index != i)
+        {
+            int temp = a[i];
+            a[i] = a[min_index];
+            a[min_index] = temp;
+        }
+    }
+    printf("Sorted Array: ");
+    for(i = 0; i < n; i++)  {
+        printf(" %d", a[i]);
+    }
+}
+
+output
+Enter number of elements of the array :5
+Enter an element of the array:47
+Enter an element of the array:34
+Enter an element of the array:21
+Enter an element of the array:89
+Enter an element of the array:12
+Sorted Array:  12 21 34 47 89
