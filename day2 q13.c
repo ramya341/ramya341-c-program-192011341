873fa444ff5215b1004ba816eda4449f73aa3b0f
ramya341 <122247132+ramya341@users.noreply.github.com>
2023-01-11 11:18:59 +0530

    Create day2 q13.c

---
+++ day2 q13.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+int main ()
+{
+    int n = 0, i = 0, largest1 = 0, largest2 = 0, temp = 0;
+ 
+    printf ("Enter the number of elements :\n");
+    scanf ("%d", &n);
+    int array[n];
+    printf ("Enter the elements :\n");
+    for (i = 0; i < n; i++)
+    {
+        scanf ("%d", &array[i]);
+    }
+ 
+    printf ("The elements are : ");
+    for (i = 0; i < n; i++)
+    {
+        printf ("%d ", array[i]);
+    }
+ 
+    printf ("\n");
+ 
+    largest1 = array[0];
+    largest2 = array[1];
+ 
+    if (largest1 < largest2)
+    {
+        temp = largest1;
+        largest1 = largest2;
+        largest2 = temp;
+    }
+ 
+    for (i = 2; i < n; i++)
+    {
+        if (array[i] > largest1)
+        {
+            largest2 = largest1;
+            largest1 = array[i];
+        }
+        else if (array[i] > largest2 && array[i] != largest1)
+        {
+            largest2 = array[i];
+        }
+    }
+ 
+    printf ("The FIRST LARGEST = %d\n", largest1);
+    printf ("THE SECOND LARGEST = %d\n", largest2);
+ 
+    return 0;
+}
+
+output
+Enter the number of elements :
+5
+Enter the elements :
+2
+4
+5
+8
+7
+The elements are : 2 4 5 8 7
+The FIRST LARGEST = 8
+THE SECOND LARGEST = 7
